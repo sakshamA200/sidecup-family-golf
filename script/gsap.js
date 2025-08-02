4708e0d9a38e84f128a23695c54502b7e5de4a33
sakshamA200 <161927082+sakshamA200@users.noreply.github.com>
2025-08-02 16:15:26 +0530

    .

--- script/gsap.js
+++ script/gsap.js
@@ -1,6 +1,6 @@
 gsap.to("#navbar",{
     backgroundColor:"#000000",
-    height:"135px",
+    height:"6vw",
     duration:0.5,
     scrollTrigger:{
         trigger:"#navbar",
@@ -23,17 +23,19 @@ gsap.to("main",{
 
 let crsr = document.getElementById("cursor");
 let blur = document.getElementById("cursor-blur");
+let li = document.querySelectorAll("li")
 
 document.addEventListener("mousemove", (e) => {
-  // Animate the main cursor
+  const cursorWidth = crsr.offsetWidth;
+  const cursorHeight = crsr.offsetHeight;
+
   gsap.to(crsr, {
-    left: e.clientX,
-    top: e.clientY,
-    duration: 0.2,   // Smaller = faster; Larger = more delay
+    left: e.clientX - cursorWidth / 2,
+    top: e.clientY - cursorHeight / 2,
+    duration: 0.2,
     ease: "power2.out"
   });
 
-  // Animate the blur effect (with more delay)
   gsap.to(blur, {
     left: e.clientX - 250,
     top: e.clientY - 250,
@@ -41,4 +43,30 @@ document.addEventListener("mousemove", (e) => {
     ease: "power3.out"
   });
 });
+
+li.forEach(link=>{
+  link.addEventListener("mouseenter",()=>{
+    gsap.to(crsr,{
+      width:"100px",
+      height:"100px",
+      backgroundColor:"transparent",
+      border:"1px solid white",
+      duration:0.2
+    })
+  })
+})
+li.forEach(link=>{
+  link.addEventListener("mouseleave", () => {
+    gsap.to(crsr, {
+      width: "20px",             
+      height: "20px",
+      backgroundColor: "#affc41",    
+      border: "none",                    
+      duration:0.2
+    });
+});
+})
+
+
+
 
